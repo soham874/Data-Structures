db5616fe71c3ae1fbf32fa4a3906eec50ff95de5
soham874 <soham874@gmail.com>
2021-01-03 19:05:44 +0530

    Basic graph operations

---
+++ src/Graph/GraphTutorial.java
@@ -0,0 +1,28 @@
+package Graph;
+
+
+public class GraphTutorial {
+	public static void main(String[] args) {
+		
+		UtilityG.pushEdge(0,1);
+		UtilityG.pushEdge(0,4);
+	
+		UtilityG.pushEdge(1,0);
+		UtilityG.pushEdge(1,4);
+		UtilityG.pushEdge(1,2);
+		UtilityG.pushEdge(1,3);
+		
+		UtilityG.pushEdge(2,1);
+		UtilityG.pushEdge(2,3);
+		
+		UtilityG.pushEdge(3,1);
+		UtilityG.pushEdge(3,4);
+		UtilityG.pushEdge(3,2);
+		
+		UtilityG.pushEdge(4,3);
+		UtilityG.pushEdge(4,0);
+		UtilityG.pushEdge(4,1);
+		
+		UtilityG.printList();
+	}
+}
---
+++ src/Graph/UtilityG.java
@@ -0,0 +1,106 @@
+package Graph;
+
+import java.util.ArrayList;
+
+public class UtilityG {
+
+	public static ArrayList<GNode> graph = new ArrayList<GNode>();
+
+	private static int nodes;
+	private static int[][] matrix = new int[nodes][nodes];
+
+	// each node of a graph
+	public static class GNode {
+		GNode next;
+		int data;
+
+		GNode(int d) {
+			data = d;
+			next = null;
+		}
+	}
+
+	// prints the adjacency matrix
+	public static void printMatrix() {
+		System.out.println("~~~~~~~~~~~ADJACENCY MATRIX~~~~~~~~~~~~~");
+		System.out.print(" ");
+		for (int i = 0; i < nodes; i++)
+			System.out.print(" " + i);
+
+		System.out.println();
+
+		for (int i = 0; i < nodes; i++) {
+
+			System.out.print(i + " ");
+
+			for (int j = 0; j < nodes; j++)
+				System.out.print(matrix[i][j] + " ");
+
+			System.out.println();
+		}
+
+	}
+
+	// push an edge to the graph
+	public static void pushEdge(int v1, int v2) {
+		System.out.println("Pushing edge between " + v1 + " and " + v2 + ".....");
+		if (v1 == v2) {
+			System.out.println("Error..attempt to create edge between same vertices");
+			return;
+		}
+		
+		checkExistance(v1);
+		checkExistance(v2);
+
+		addEdge(v1, v2);
+
+		System.out.println("Edge successfully added to graph");
+	}
+
+	// adds v2 to list for v1
+	private static void addEdge(int v1, int v2) {
+
+		GNode vertex2 = new GNode(v2);
+		
+		for (int i = 0; i < graph.size(); i++) {
+
+			if (graph.get(i).data == v1) {
+
+				GNode current = graph.get(i);
+				
+				while(current.next != null)
+					current = current.next;
+				
+				current.next = vertex2;			
+			}
+		}
+
+	}
+
+	// checks if the data exists in graph
+	private static void checkExistance(int v) {
+
+		for (int i = 0; i < graph.size(); i++) {
+			if (graph.get(i).data == v)
+				return;
+		}
+			
+		
+		GNode current = new GNode(v);
+		graph.add(current);
+	}
+
+	// prints the adjacency list
+	public static void printList() {
+		System.out.println("~~~~~~~~~~~ADJACENCY LIST~~~~~~~~~~~~~");
+		for (int i = 0; i < graph.size(); i++) {
+			GNode current = graph.get(i);
+			while (current != null) {
+				System.out.print(current.data + " -> ");
+				current = current.next;
+			}
+			System.out.println();
+		}
+	}
+
+}
